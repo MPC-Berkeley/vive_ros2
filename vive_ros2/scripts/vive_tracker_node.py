102eeacffe094bc8b76d7104862abd24400ed7b4
Michael <michaelrequi@gmail.com>
2020-12-06 15:24:13 -0800

    Added node with odom publishing

---
+++ vive_ros2/scripts/vive_tracker_node.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from rclpy.qos import qos_profile_sensor_data
+import tf2_ros
+
+from threading import Thread, Event
+from queue import Queue
+
+from vive_tracker_client import ViveTrackerClient
+
+
+class ViveTrackerNode(Node):
+
+    def __init__(self):
+        super().__init__('vive_tracker_node')
+        self.declare_parameter('host_ip', '192.168.50.171')
+        self.declare_parameter('host_port', 8000)
+        self.declare_parameter('tracker_name', 'tracker_1')
+        self.declare_parameter('parent_link_name', 'odom')
+        self.declare_parameter('link_name', 'tracker_link')
+
+        (self.host_ip, self.host_port, self.tracker_name, self.parent_link_name, self.link_name) = self.get_parameters(
+            ['host_ip', 'host_port', 'tracker_name', 'parent_link_name', 'link_name'])
+
+        self.odom_pub = self.create_publisher(Odometry, 'tracker/odom', qos_profile=qos_profile_sensor_data)
+
+        client = ViveTrackerClient(host=self.host_ip.get_parameter_value().string_value,
+                                   port=self.host_port.get_parameter_value().integer_value,
+                                   tracker_name=self.tracker_name.get_parameter_value().string_value,
+                                   should_record=False)
+
+        self.message_queue = Queue()
+        self.kill_thread = Event()
+        self.client_thread = Thread(target=client.run_threaded, args=(self.message_queue, self.kill_thread,))
+
+        try:
+            self.client_thread.start()
+
+            while rclpy.ok():
+                msg = self.message_queue.get()
+
+                odom_msg = Odometry()
+                odom_msg.header.stamp = self.get_clock().now().to_msg()
+                odom_msg.header.frame_id = self.link_name.get_parameter_value().string_value
+
+                odom_msg.child_frame_id = self.parent_link_name.get_parameter_value().string_value
+
+                odom_msg.pose.pose.position.x = msg.x
+                odom_msg.pose.pose.position.y = msg.y
+                odom_msg.pose.pose.position.z = msg.z
+
+                odom_msg.twist.twist.linear.x = msg.vel_x
+                odom_msg.twist.twist.linear.y = msg.vel_y
+                odom_msg.twist.twist.linear.z = msg.vel_z
+
+                self.odom_pub.publish(odom_msg)
+
+        finally:
+            # cleanup
+            self.kill_thread.set()
+            self.client_thread.join()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    vive_tracker_node = ViveTrackerNode()
+    rclpy.spin(vive_tracker_node)
+    vive_tracker_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
