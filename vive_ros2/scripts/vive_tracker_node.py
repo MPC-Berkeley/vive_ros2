da2a00f179a8aa5fd83d94a2c43bebaeded34a1b
Michael Equi <michaelrequi@gmail.com>
2021-02-11 13:04:52 -0800

    Added topic parameter

--- vive_ros2/scripts/vive_tracker_node.py
+++ vive_ros2/scripts/vive_tracker_node.py
@@ -18,14 +18,17 @@ class ViveTrackerNode(Node):
         self.declare_parameter('host_ip', '192.168.50.171')
         self.declare_parameter('host_port', 8000)
         self.declare_parameter('tracker_name', 'T_1')
+        self.declare_parameter('topic', '')
         self.declare_parameter('link_name', 'odom')
         self.declare_parameter('child_link_name', 'tracker_link')
 
-        (self.host_ip, self.host_port, self.tracker_name, self.link_name, self.child_link_name) = self.get_parameters(
-            ['host_ip', 'host_port', 'tracker_name', 'link_name', 'child_link_name'])
+        (self.host_ip, self.host_port, self.tracker_name, self.link_name, self.child_link_name, self.topic) = self.get_parameters(
+            ['host_ip', 'host_port', 'tracker_name', 'link_name', 'child_link_name', 'topic'])
 
-        self.odom_pub = self.create_publisher(Odometry, self.tracker_name.get_parameter_value().string_value + '/odom',
-                                              qos_profile=qos_profile_sensor_data)
+        topic = self.topic.get_parameter_value().string_value
+        topic_name = self.tracker_name.get_parameter_value().string_value + '/odom' if topic == "" else topic
+        self.odom_pub = self.create_publisher(Odometry, topic_name,
+            qos_profile=qos_profile_sensor_data)
 
         client = ViveTrackerClient(host=self.host_ip.get_parameter_value().string_value,
                                    port=self.host_port.get_parameter_value().integer_value,
